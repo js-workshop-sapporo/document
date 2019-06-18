367be1d903853cbcf4b46ce9873edfb5fe72c42d
redamoon <yuuki60824@gmail.com>
2019-06-19 02:31:21 +0900

    [add] devtool ブレークポイントの処理追加

--- devtool/example/app.js
+++ devtool/example/app.js
@@ -3,10 +3,26 @@ console.log('Hello Developer Tool');
 // 変数の確認
 const siam_shade = 'JS Workshop Sapporo';
 
-const ss_member = ['HIDEKI', 'KAZUMA', 'DAITA', 'NATCHIN', '淳士']
+const ss_member = ['HIDEKI', 'KAZUMA', 'DAITA', 'NATCHIN', '淳士', 1, ['配列1', '配列2'], {name: '名前', age: 18}]
 let new_member = ss_member.map( value => {
+  typeCheck(value)
   return value
 })
 
 // SIAM SHADEの6人目のメンバーを追加する
 new_member.push('グッチー')
+
+function typeCheck(type) {
+  let types = type
+  switch(typeof type) {
+    case 'string':
+      console.log('文字列')
+      break
+    case 'number':
+      console.log('数字')
+      break
+    case 'object':
+      console.log('オブジェクト')
+      break
+  }
+}
