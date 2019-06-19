532e17df69f754ac0f94fb02a811458e0e1b2554
nagasawaaaa <snagasaw.sp.create@gmail.com>
2019-06-20 01:09:59 +0900

    add: 上級者向けの使い方

--- devtool/example2/app.js
+++ devtool/example2/app.js
@@ -4,12 +4,12 @@ const num1 = document.getElementById('num1');
 const num2 = document.getElementById('num2');
 
 function calc (num1, num2) {
-  result.innerText = Number(num1) + Number(num2)
+  result.innerText = num1 + num2
 }
 
 calcButton.addEventListener('click', () => {
-  const num1Val = num1.value;
-  const num2Val = num2.value;
+  const num1Val = Number(num1.value);
+  const num2Val = Number(num2.value);
   calc(num1Val, num2Val);
 });
 
